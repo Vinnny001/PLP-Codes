86be8e463870ccba9221d6c890936d0fb0f24194
Vincent Otieno Ogweno <ogwenovincent00@gmail.com>
2024-04-28 00:57:53 +0300

    added Python wk1 assignment

---
+++ ASSIGNMENTS/Python/user_input.py.py
@@ -0,0 +1,11 @@
+# Ask the user for their name
+name = input("What is your name?\n")
+
+# Ask the user for their age
+age = input("How old are you?\n")
+
+# Ask the user for their location
+location = input("Where do you live?\n")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
